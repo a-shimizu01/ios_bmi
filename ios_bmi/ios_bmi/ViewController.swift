8d5da6d2292e477d137f53a3f8bb97d6c29f275f
a-shimizu <a-shimizu@cries.co.jp>
2019-11-25 15:14:30 +0900

    ・入力フィールド配置 ・BMI計算処理実装 ・削除ボタンタップ時の画面クリア処理実装

--- ios_bmi/ios_bmi/ViewController.swift
+++ ios_bmi/ios_bmi/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldHeight: UITextField!
+    @IBOutlet weak var textFieldWeight: UITextField!
+    @IBOutlet weak var labelBmiValue: UILabel!
+    @IBOutlet weak var textViewExplanation: UITextView!
+    
+    private let textViewDefaultStr = "言い訳を書きたければ書いてください"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func bmiCalculateButtonTouchDown(_ sender: UIButton) {
+        guard let textFieldHeightStr = textFieldHeight.text,
+            let textFieldWeightStr = textFieldWeight.text,
+            let heightDouble = Double(textFieldHeightStr),
+            let weightDouble = Double(textFieldWeightStr) else {
+                // アラートダイアログ エラーで落ちるので一旦保留
+//                showAlertDialog(title: "エラー", message: "入力値が正しくありません。")
+                labelBmiValue.text = ""
+                return
+        }
+        
+        let bmiValue = bmiCalculate(heightCm: heightDouble, weight: weightDouble)
+        labelBmiValue.text = String(bmiValue)
+        
+    }
+    
+    @IBAction func bmiClearButtonTouchDown(_ sender: UIButton) {
+        textFieldHeight.text = ""
+        textFieldWeight.text = ""
+        labelBmiValue.text = ""
+        textViewExplanation.text = textViewDefaultStr
+    }
+    
+    
+    @IBAction func bmiSaveButtonTouchDown(_ sender: UIButton) {
+        
+        // アラートダイアログ エラーで落ちるので一旦保留
+//        showAlertDialog(title: "エラー", message: "BMI値が未入力です。")
+        
+        labelBmiValue.text = "保存する"
+    }
+    
+    private func bmiCalculate(heightCm: Double, weight: Double) -> Double {
+        let heightM = heightCm / 100
+        let bmi = weight / pow(heightM, 2)
+        return bmi
+    }
+    
+    private func showAlertDialog(title: String, message: String) {
+        let dialog = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        dialog.addAction(UIAlertAction(title: "OK",  style: .cancel, handler: nil))
+        dialog.present(dialog,animated: true, completion: nil)
+    }
 }
 
