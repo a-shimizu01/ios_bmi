6e580b2cda7cf411442b670616382f4a37fdd565
a-shimizu <a-shimizu@cries.co.jp>
2019-12-10 13:03:28 +0900

    BMI登録データのテストデータ登録処理追加。リスト画面の暫定実装。

--- ios_bmi/ios_bmi/InputViewController.swift
+++ ios_bmi/ios_bmi/InputViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InputViewController: UIViewController {
 
     @IBOutlet weak var textFieldHeight: UITextField!
     @IBOutlet weak var textFieldWeight: UITextField!
     @IBOutlet weak var labelBmiValue: UILabel!
     @IBOutlet weak var textViewExplanation: UITextView!
     
+    static let HEIGHT_KEY = "height"
+    static let WEIGHT_KEY = "weight"
+    static let BMI_KEY = "bmi"
+    static let EXPLANATION_KEY = "explanation"
+    
     private let textViewDefaultStr = "言い訳を書きたければ書いてください"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // テスト用データ登録
+        saveBmiTestData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -55,7 +63,47 @@ class ViewController: UIViewController {
         // アラートダイアログ エラーで落ちるので一旦保留
 //        showAlertDialog(title: "エラー", message: "BMI値が未入力です。")
         
-        labelBmiValue.text = "保存する"
+        // データ保存用Dictionary
+        let dataDictionary = [InputViewController.HEIGHT_KEY: textFieldHeight.text, InputViewController.WEIGHT_KEY: textFieldWeight.text, InputViewController.BMI_KEY: labelBmiValue.text, InputViewController.EXPLANATION_KEY: textViewExplanation.text]
+        
+        // 現在日付取得
+        let formatter = DateFormatter()
+        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM", options: 0, locale: Locale(identifier: "ja_JP"))
+        let todayDateStr = formatter.string(from: Date())
+        
+        // 現在日付をキーにDictionaryを保存する
+        UserDefaults.standard.set(dataDictionary, forKey: todayDateStr)
+        
+        // アラートダイアログ エラーで落ちるので一旦保留）（保存しましたってダイアログ出るようにしたいなぁ）
+        labelBmiValue.text = "保存完了"
+    }
+    
+    // テスト用メソッド 複数件のBMI計算結果のテストデータを保存する
+    func saveBmiTestData() {
+        let testDataKey1 = "2019年10月10日"
+        let testDataDictionary1 = [InputViewController.HEIGHT_KEY: "160.0", InputViewController.WEIGHT_KEY: "55.0", InputViewController.BMI_KEY: "21.48", InputViewController.EXPLANATION_KEY: "10月1日目です。"]
+        
+        let testDataKey2 = "2019年10月25日"
+        let testDataDictionary2 = [InputViewController.HEIGHT_KEY: "160.1", InputViewController.WEIGHT_KEY: "55.1", InputViewController.BMI_KEY: "21.49", InputViewController.EXPLANATION_KEY: "10月2日目です。"]
+        
+        let testDataKey3 = "2019年11月15日"
+        let testDataDictionary3 = [InputViewController.HEIGHT_KEY: "160.2", InputViewController.WEIGHT_KEY: "55.2", InputViewController.BMI_KEY: "21.50", InputViewController.EXPLANATION_KEY: "11月1日目です。"]
+        
+        let testDataKey4 = "2019年11月20日"
+        let testDataDictionary4 = [InputViewController.HEIGHT_KEY: "160.3", InputViewController.WEIGHT_KEY: "55.3", InputViewController.BMI_KEY: "21.52", InputViewController.EXPLANATION_KEY: "11月2日目です。"]
+        
+        let testDataKey5 = "2019年12月3日"
+        let testDataDictionary5 = [InputViewController.HEIGHT_KEY: "160.4", InputViewController.WEIGHT_KEY: "55.4", InputViewController.BMI_KEY: "21.53", InputViewController.EXPLANATION_KEY: "12月1日目です。"]
+        
+        let testDataKey6 = "2019年12月8日"
+        let testDataDictionary6 = [InputViewController.HEIGHT_KEY: "160.5", InputViewController.WEIGHT_KEY: "55.5", InputViewController.BMI_KEY: "21.54", InputViewController.EXPLANATION_KEY: "12月2日目です。"]
+        
+        UserDefaults.standard.set(testDataDictionary1, forKey: testDataKey1)
+        UserDefaults.standard.set(testDataDictionary2, forKey: testDataKey2)
+        UserDefaults.standard.set(testDataDictionary3, forKey: testDataKey3)
+        UserDefaults.standard.set(testDataDictionary4, forKey: testDataKey4)
+        UserDefaults.standard.set(testDataDictionary5, forKey: testDataKey5)
+        UserDefaults.standard.set(testDataDictionary6, forKey: testDataKey6)
     }
     
     // 入力エリア以外をタップするとキーボードを閉じる
@@ -63,6 +111,7 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    // BMI計算処理
     private func bmiCalculate(heightCm: Double, weight: Double) -> Double {
         let heightM = heightCm / 100
         let bmi = weight / pow(heightM, 2)
--- ios_bmi/ios_bmi/ListViewController.swift
+++ ios_bmi/ios_bmi/ListViewController.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+//    @IBOutlet weak var dateLabel: UILabel!
+//    @IBOutlet weak var heightLabel: UILabel!
+//    @IBOutlet weak var weightLabel: UILabel!
+//    @IBOutlet weak var bmiLabel: UILabel!
+//    @IBOutlet weak var explanationLabel: UILabel!
+    
+    @IBOutlet weak var bmiDataTableView: UITableView!
+    private let BMI_DATA_TABLE_CELL_IDENTIFIER = "bmiDataTableCell"
+    
+    let fruits = ["apple", "banana", "grape"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM", options: 0, locale: Locale(identifier: "ja_JP"))
+        let todayDateStr = formatter.string(from: Date())
+        
+        
+        
+//        guard let dataDictionary = UserDefaults.standard.dictionary(forKey: todayDateStr) else {
+//            dateLabel.text = todayDateStr
+//            heightLabel.text = "データがnil"
+//            weightLabel.text = "データがnil"
+//            bmiLabel.text = "データがnil"
+//            explanationLabel.text = "データがnil"
+//            return
+//        }
+//        let heightStr = dataDictionary[InputViewController.HEIGHT_KEY] as? String ?? ""
+//        let weightStr = dataDictionary[InputViewController.WEIGHT_KEY] as? String ?? ""
+//        let bmiStr = dataDictionary[InputViewController.BMI_KEY] as? String ?? ""
+//        let explanationStr = dataDictionary[InputViewController.EXPLANATION_KEY] as? String ?? ""
+//
+//        dateLabel.text = todayDateStr
+//        heightLabel.text = heightStr
+//        weightLabel.text = weightStr
+//        bmiLabel.text = bmiStr
+//        explanationLabel.text = explanationStr
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let bmiDataTableCell: UITableViewCell = bmiDataTableView.dequeueReusableCell(withIdentifier: BMI_DATA_TABLE_CELL_IDENTIFIER, for: indexPath)
+        
+        // セルに表示する値を設定する
+        bmiDataTableCell.textLabel!.text = fruits[indexPath.row]
+        
+        return bmiDataTableCell
+    }
 
     /*
     // MARK: - Navigation
